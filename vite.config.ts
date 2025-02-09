ab824e1e5c0bc3c38e6acaeebe192e73be8c7055
Leonid Safonov <safonov.leonid3@wb.ru>
2025-02-09 19:32:07 +0300

    [dev]: set another path

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: '/',
+    base: '/Tool-kit-test/',
     plugins: [react()],
     resolve: {
         alias: {
